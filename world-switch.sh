f224819846549980515e42ba34680a7ab3a2ea67
Munkenhagg <munkenhagg@gmail.com>
2025-12-25 15:07:53 +0100

    Update world seitcher

--- world-switch.sh
+++ world-switch.sh
@@ -1,16 +1,5 @@
-read -p "What server do you wish to switch the active world for " server
-server="$(echo "$server" | tr '[:upper:]' '[:lower:]')"
-if [ ! -d "$(dirname $0)/$server" ]; then
-	echo "Error: No such server"
-	exit 1
-fi
-
-read -p "Enter the world name(case-sensitive) " world
-
-if [ -d "$server/$world" ]; then
-	echo "changing..."
-	sed -i "s/^level-name=.*/level-name=$world/" "$server/server.properties"
-else
-	echo -e "adding new world '$world'"
-	sed -i "s/^level-name=.*/level-name=$world/" "$server/server.properties"
-fi
+echo "Enter the server name(Case sensitive): "
+read S
+echo "Enter the World name to switch to"
+read WN
+sed -i "s/^level-name=.*/level-name=$WN/" "$S/server.properties"
